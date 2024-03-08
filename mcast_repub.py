55842c1041e5fead3be8c6fc5ce030eb77cc94b1
Brian Scott Hamilton <bham@Boru.attlocal.net>
2024-03-07 21:02:57 -0600

    added input validation

--- mcast_repub.py
+++ mcast_repub.py
@@ -1,35 +1,81 @@
+import argparse
 import socket
 import struct
+import psutil
 
-MCAST_GRP_RCV = '224.0.0.94'
-MCAST_PORT_RCV = 5007
-MCAST_GRP_REPUB = '224.0.0.98'
-MCAST_PORT_REPUB = 5008
-# IS_ALL_GROUPS = True
-IS_ALL_GROUPS = False
+def validate_multicast_ip(ip):
+ 
+  octets = ip.split('.')
+  if len(octets) == 4:
+    if 224 <= int(octets[0]) <= 239:
+      for octet in octets[1:]:
+        if 0 <= int(octet) <= 255:
+          return True
+  print(f'error: {ip} is not a valid multicast ip address (not in 224.0.0.0/4)')
+  return False
+         
+
+def validate_mgroups(args):
+
+  print(args)
+
+  if args.mgroup_listen == args.mgroup_repub:
+    print(f'error: arguments mgroup-listen and mgroup-repub cannot be the same (both are \'{args.mgroup_listen}\')')
+    exit(1)
+
+  if not (validate_multicast_ip(args.mgroup_listen) and validate_multicast_ip(args.mgroup_repub)):
+    exit(2)
+
+
+def validate_interface(iface):
+
+  psutil.net_if_addrs()
+  ifaces = psutil.net_if_addrs()
+
+  if iface not in ifaces.keys():
+    print(f'error: interface {iface} does not exist on this host')
+    exit(3)    
+  
+  if ifaces[iface][0].family == socket.AF_INET:
+    return ifaces[iface][0].address
+  else:
+    print(f'error: interface {iface} is not type AF_INET')
+    print(f'the following interfaces on this host are valid:')
+    for iface in ifaces:
+      if ifaces[iface][0].family == socket.AF_INET:
+        print(f'\t{iface}: {ifaces[iface][0].address}')
+    exit(4)
+
 
 def main():
-    sock_rcv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    sock_rcv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    sock_repub = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    sock_repub.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-i', '--interface', required=True)
+  parser.add_argument('-l', '--mgroup-listen', required=True)
+  parser.add_argument('-r', '--mgroup-repub',required=True)
+  parser.add_argument('-p', '--port',type=int,required=True)
 
+  args = parser.parse_args()
 
-    if IS_ALL_GROUPS:
-        print(f'Binded to 0.0.0.0:{MCAST_PORT_RCV}')
-        sock_rcv.bind(('', MCAST_PORT_RCV))
-    else:
-        print(f'Binded to {MCAST_GRP_RCV}:{MCAST_PORT_RCV}')
-        sock_rcv.bind((MCAST_GRP_RCV, MCAST_PORT_RCV))
+  validate_mgroups(args)
+  listen_iface = validate_interface(args.interface)
+      
+  # create UDP socket
+  sock_rcv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+  sock_rcv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP_RCV), socket.INADDR_ANY)
+  print(f'Binding to {args.mgroup_listen}:{args.port}')
+  sock_rcv.bind((args.mgroup_listen, args.port))
 
-    sock_rcv.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+  mreq = struct.pack("4s4s", socket.inet_aton(args.mgroup_listen), socket.inet_aton(listen_iface))
+  sock_rcv.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-    while True:
-      message = sock_rcv.recv(4096)
-      sock_repub.sendto(message, (MCAST_GRP_REPUB, MCAST_PORT_REPUB))
+  sock_repub = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+  sock_repub.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+
+  while True:
+    message = sock_rcv.recv(4096)
+    sock_repub.sendto(message, (args.mgroup_repub, args.port))
 
 
 if __name__ == '__main__':
